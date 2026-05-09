404c010910cada266c9134774b3175a20b0afd0b
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2026-05-09 15:46:02 +0300

    finish

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,22 @@
 'use strict';
 
-// write your code here
 const logo = document.querySelector('.logo');
+
+const handleSuccess = (message) => {
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.textContent = message;
+  document.body.appendChild(div);
+};
+
+const handleError = (error) => {
+  const div = document.createElement('div');
+
+  div.classList.add('message', 'error-message');
+  div.textContent = error.message;
+  document.body.appendChild(div);
+};
 
 const promise1 = new Promise((resolve) => {
   logo.addEventListener('click', () => {
@@ -11,22 +26,9 @@ const promise1 = new Promise((resolve) => {
 
 const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    reject(new Error(`Promise was rejected!`));
+    reject(new Error('Promise was rejected!'));
   }, 3000);
 });
 
-promise1.then((message) => {
-  const div = document.createElement('div');
-
-  div.classList.add('message');
-  div.textContent = message;
-  document.body.appendChild(div);
-});
-
-promise2.catch((message) => {
-  const div = document.createElement('div');
-
-  div.classList.add('message', 'error-message');
-  div.textContent = message;
-  document.body.appendChild(div);
-});
+promise1.then(handleSuccess).catch(handleError);
+promise2.then(handleSuccess).catch(handleError);
